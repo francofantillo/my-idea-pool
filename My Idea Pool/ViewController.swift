976af3e89ab8810b4afb21aabefb6689ac97f89b
francofantillo <franco_fantillo@hotmail.com>
2019-10-29 10:57:39 -0700

    Initial Commit

---
+++ My Idea Pool/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My Idea Pool
+//
+//  Created by Franco on 2019-10-29.
+//  Copyright © 2019 Franco Fantillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
